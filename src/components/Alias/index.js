47b6dc0f9a14c1a58389a534b880cd0d58957fce
audreydetsaboun <audrey.detsaboun@gmail.com>
2020-08-18 10:33:43 +0200

    add of the Alias component + add of the react router dom

---
+++ src/components/Alias/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.scss';
+
+function Alias() {
+  return (
+    <div className="alias">
+      <h1>Composant Alias</h1>
+    </div>
+  );
+}
+
+export default Alias;
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,25 +1,37 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import logo from './logo.svg';
 import './style.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route exact path="/logon">
+            <Registration />
+          </Route>
+
+          <Route exact path="/login">
+            <Connexion />
+          </Route>
+
+          <Route exact path="/play">
+            <Game />
+          </Route>
+
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
