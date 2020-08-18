98a1f250295801a2bc556106e60bc4bc45ff3722
JeremyThiebaut <jeremy.thiebaut.pro@gmail.com>
2020-08-18 15:58:29 +0200

    Start Game component

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './style.scss';
+import Connection from './Connection';
+import Menu from './Menu';
 
 function Game() {
   return (
     <div className="game">
       <h1>Composant Game</h1>
+      <Connection />
+      <Tag />
+      <Menu />
+      <Headband />
+      <Text />
+      <Selection />
+      <ButtonNext />
     </div>
   );
 }
