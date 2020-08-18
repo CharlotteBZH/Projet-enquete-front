8bf971c2a655b61f82d3d42c42d11a3370c7d102
audreydetsaboun <audrey.detsaboun@gmail.com>
2020-08-18 19:27:50 +0200

    set up jsx of Registration component, adding of the reset.css file in src

--- src/components/Registration/index.js
+++ src/components/Registration/index.js
@@ -1,10 +1,43 @@
 import React from 'react';
 import './style.scss';
 
-function Registration() {
+function Registration(props) {
   return (
     <div className="registration">
-      <h1>S'inscrire</h1>
+      <h1>Formulaire d'inscription</h1>
+
+      <form>
+        <input
+          type="text"
+          value=""
+          placeholder="Entrez votre pseudo"
+        />
+        <input
+          type="mail"
+          value=""
+          placeholder="Entrez votre mail"
+        />
+        <input
+          type="mail"
+          value=""
+          placeholder="Confirmez votre mail"
+        />
+        <input
+          type="text"
+          value=""
+          placeholder="Entrez votre mot de passe"
+        />
+        <input
+          type="text"
+          value=""
+          placeholder="Confirmez votre mot de passe"
+        />
+        <input
+          type="submit"
+          value="S'inscrire"
+        />
+      </form>
+
     </div>
   );
 }
