4d1d7fe5eee91542b57d9db8f90cb6513313c1ef
audreydetsaboun <audrey.detsaboun@gmail.com>
2020-08-19 11:25:29 +0200

    added file styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
-import './reset.css';
+import './styles/index.scss';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
