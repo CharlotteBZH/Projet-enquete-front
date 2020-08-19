051d97b421c3271c2eb8a9baf6ff579b7a76b9d6
audreydetsaboun <audrey.detsaboun@gmail.com>
2020-08-19 10:59:25 +0200

    added the link from / to /play

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import './style.scss';
 
 import Alias from '../Alias';
@@ -12,13 +12,15 @@ function Home({ connected }) {
       <span className="title-game">
         <h1>Enquête CLUED'O</h1>
       </span>
-      <span className="play-game">
+
+      <Link to={`/play`} >
         <button
           className="play"
           type="button">
           Jouer
         </button>
-      </span>
+      </Link>
+
       <div className="tolog">
         {connected ? <Alias /> : <Log />}
         {console.log(connected)}
