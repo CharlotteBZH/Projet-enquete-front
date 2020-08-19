6e7984688939e74295b39a58d1919ac754a1f618
audreydetsaboun <audrey.detsaboun@gmail.com>
2020-08-19 11:30:18 +0200

    added of the content in Connexion

--- src/components/Connexion/index.js
+++ src/components/Connexion/index.js
@@ -5,7 +5,31 @@ function Connexion() {
   return (
     <div className="connexion">
       <h1>Se connecter</h1>
+
+      <form className="registration-form">
+
+        <input
+          type="mail"
+          value=""
+          placeholder="Entrez votre mail"
+          required
+        />
+
+        <input
+          type="text"
+          value=""
+          placeholder="Entrez votre mot de passe"
+          required
+        />
+
+        <input className="registration-form__submit"
+          type="submit"
+          value="Se connecter"
+        />
+      </form>
+
     </div>
+
   );
 }
 
