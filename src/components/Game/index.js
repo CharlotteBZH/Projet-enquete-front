0afbbc44526b5606e004014c1144c8bc9b19ca45
Lescuclai <lea-escudie@hotmail.fr>
2020-08-19 11:56:35 +0200

    beginning of Game componenent

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,21 +1,71 @@
 import React from 'react';
 import './style.scss';
-import Connection from './Connection';
-import Menu from './Menu';
+import './headBand.scss';
+import './buttonNext.scss';
+import './text.scss';
+import './selection.scss';
+
+
+import Connexion from '../Connexion';
+//import Menu from '../Menu';
 
 function Game() {
   return (
     <div className="game">
       <h1>Composant Game</h1>
-      <Connection />
-      <Tag />
-      <Menu />
+      <Connexion />
+      <Tag tagName="toto"/>
       <Headband />
-      <Text />
-      <Selection />
-      <ButtonNext />
     </div>
   );
 }
 
 export default Game;
+
+
+
+
+function Tag({tagName}) {
+  return (
+    <div className="tag">
+      <h2>{tagName}</h2>
+    </div>
+  );
+}
+
+function Headband() {
+  return (
+    <div className="headband">
+      <Text naration="Hello World"/>
+      <Selection question="tu penses que...?"/>
+      <ButtonNext />
+    </div>
+  );
+}
+
+function Text({naration}) {
+  return (
+    <div className="text">
+      <p>{naration}</p>
+    </div>
+  );
+}
+
+function Selection({question}) {
+  return (
+    <div className="selection">
+      <p className="selection_para">{question}</p>
+      <p className="selection_para">{question}</p>
+      <p className="selection_para">{question}</p>
+      <p className="selection_para">{question}</p>
+    </div>
+  );
+}
+
+function ButtonNext() {
+  return (
+    <div className="buttonNext">
+      <h1>></h1>
+    </div>
+  );
+}
