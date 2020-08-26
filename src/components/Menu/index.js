dfb7a97a55155477b9c8edb03185390461d32843
CharlotteBZH <c.lepeigneul@gmail.com>
2020-08-26 16:14:50 +0200

    begining of Menu/Redux

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,19 +1,22 @@
 import React from "react";
 // import { Link } from "react-router-dom";
-import { Icon } from "semantic-ui-react";
+//import { Icon } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
 import "./style.scss";
 
 //import Menu from "../../containers/Menu";
 
-const Menu = ({toto}) => {
+const Menu = ({onOpenMenu, open}) => {
   return (
     <div>
-      <button className="menuButton" onClick={toto}>
+      <button className="menuButton" onClick={onOpenMenu}>
         Menu
       </button>
+      {open && <div><button>Accueil</button> <button>Mute</button> <button>Notebook</button></div>}
     </div>
   );
 };
 
 export default Menu;
+
+// {opened && loading && <div className="loading">Connexion...</div>}
